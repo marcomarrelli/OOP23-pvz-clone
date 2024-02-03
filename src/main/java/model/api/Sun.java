513e3d15fd0e70af9d1ae428a6466ccc8574ee43
Sofia Caberletti <sofia.caberletti@studio.unibo.it>
2024-02-03 10:07:32 +0100

    Added Sun interface and SunImpl class

---
+++ src/main/java/model/api/Sun.java
@@ -0,0 +1,5 @@
+package model.api;
+
+public interface Sun extends Entities {
+    
+}
---
+++ src/main/java/model/impl/SunImpl.java
@@ -0,0 +1,25 @@
+package model.impl;
+
+import model.api.Sun;
+
+public class SunImpl implements Sun {
+
+    @Override
+    public void getPosition() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPosition'");
+    }
+
+    @Override
+    public boolean isAlive() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isAlive'");
+    }
+
+    @Override
+    public String getEntityName() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getEntityName'");
+    }
+    
+}
