2f3032a3a5f7f0a138d152d6b92c6ad5c05de6b0
Margherita <margherita.zanchini@studio.unibo.it>
2024-02-03 01:09:06 +0100

    added code to the interfaces Plant and Bullet

---
+++ src/main/java/model/api/Bullet.java
@@ -0,0 +1,11 @@
+package model.api;
+
+public interface Bullet extends Entities {
+
+    /**
+     * 
+     * @return the speed of the bullet
+     */
+    public double getSpeed();
+    
+}
---
+++ src/main/java/model/api/Plants.java
@@ -0,0 +1,11 @@
+package model.api;
+
+public interface Plants extends EntitiesFight {
+
+    /**
+     * 
+     * @return the time in seconds between the attacks
+     */
+    public double getTimeBetweenAttacks();
+    
+}
