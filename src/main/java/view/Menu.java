a1a3185c7336d0b630cddb6ad5d5743a8340ece4
marcomarrelli <marcomarrelli03@gmail.com>
2024-02-03 16:43:54 +0100

    Added Starting Menu
    Added Fullscreen Option (WIP)
    
    Added Exit Option

--- src/main/java/view/Menu.java
+++ src/main/java/view/Menu.java
@@ -1,32 +1,101 @@
 package view;
 
-import java.net.URL;
-
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.application.Platform;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
+/**
+ * Starting Menu.
+ * Start Game, Fullscreen Option and Exit Buttons are available.
+ * 
+ * @author Marco Marrelli, Sofia Caberletti
+ */
 public class Menu extends Application {
-    //private final int SCREEN_WIDTH = Screen.getPrimary().getWidth();
+    //private final double SCREEN_WIDTH = Screen.getPrimary().getBounds().getWidth();
+    //private final double SCREEN_HEIGHT = Screen.getPrimary().getBounds().getHeight();
+    private double APPLICATION_WIDTH = 800;
+    private double APPLICATION_HEIGHT = 600;
+    private final double MARGINS = Math.min(APPLICATION_WIDTH*0.03, APPLICATION_HEIGHT*0.03);
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        FXMLLoader fileLoader = new FXMLLoader(getClass().getResource("/menuBackground.fxml"));
-        Parent background = fileLoader.load();
+    private final String FULLSCREEN_KEY_COMBINATION = "ESC";
+    private final String FULLSCREEN_MESSAGE = "Press " + FULLSCREEN_KEY_COMBINATION + " to exit Fullscreen Mode!";
+    private final String SCENE_BACKGROUND = "menuBackground.jpeg";
+    private final String BUTTON_BACKGROUND = "tombstoneTexture.jpg";
 
-        primaryStage.setTitle("Plants Vs Zombies");
-        primaryStage.setScene(new Scene(background, 300, 250));
-        primaryStage.show();
+    /**
+     * Initialize the Stage, settings his bounds and properties.
+     * 
+     * @param stage the stage to be initialized.
+     * @author Marco Marrelli
+     */
+    private void initStage(Stage stage) {
+        stage.setTitle("Plants Vs Zombies");
+        stage.setResizable(false);
+        stage.setWidth(APPLICATION_WIDTH);
+        stage.setHeight(APPLICATION_HEIGHT);
+
+        stage.setFullScreenExitHint(FULLSCREEN_MESSAGE);
+        stage.setFullScreenExitKeyCombination(KeyCombination.valueOf(FULLSCREEN_KEY_COMBINATION));
     }
 
-    public static void main(String[] args) {
-        Application.launch(args);
+    /**
+     * Initialize the menu buttons group.
+     * 
+     * @param buttons menu buttons to be added.
+     * @return VBox containing the menu buttons.
+     * @author Marco Marrelli
+     */
+    private VBox initMenuButtons(Button... buttons)  {
+        VBox box = new VBox(MARGINS, buttons);
+
+        if(box.getChildren().isEmpty()) {
+            return null;
+        }
+
+        //if(!Files.exists(Paths.get(SCENE_BACKGROUND))) throw new FileNotFoundException("File " + BUTTON_BACKGROUND + " was not found in your System!");
+
+        box.getChildren().forEach(b -> {
+            VBox.setVgrow(((Button) b), Priority.ALWAYS);
+            ((Button) b).setStyle("-fx-background-image: url(\'" + BUTTON_BACKGROUND + "\'); -fx-text-fill: #ffffff; -fx-font-size: 2em; -fx-background-size: stretch;");
+            ((Button) b).setMaxWidth(Double.MAX_VALUE);
+            ((Button) b).setMaxHeight(APPLICATION_HEIGHT*0.1);
+        });
+
+        box.setPrefHeight(APPLICATION_HEIGHT);
+        box.setMaxWidth(APPLICATION_WIDTH*0.5);
+        box.setSpacing(APPLICATION_HEIGHT*0.05);
+        box.setAlignment(Pos.CENTER);
+
+        return box;
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        initStage(stage);
+
+        BorderPane root = new BorderPane();
+        root.setStyle("-fx-background-image: url(\'" + SCENE_BACKGROUND + "\'); -fx-background-size: cover;");
+        //if(!Files.exists(Paths.get(SCENE_BACKGROUND))) throw new FileNotFoundException("File " + SCENE_BACKGROUND + " was not found in your System!");
+
+        Button startButton = new Button("Start Adventure");
+        
+        Button fullscreenButton = new Button("Fullscreen (Settings)");
+        fullscreenButton.setOnAction(e -> stage.setFullScreen(!stage.isFullScreen()));
+
+        Button exitButton = new Button("Exit");
+        exitButton.setOnAction(e -> Platform.exit());
+
+        VBox menuGroup = initMenuButtons(startButton, fullscreenButton, exitButton);
+
+        root.setCenter(menuGroup);
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
