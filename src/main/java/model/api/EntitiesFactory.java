7616deb5c8b35f828ce854ff46a376a07157ae37
marcomarrelli <marcomarrelli03@gmail.com>
2024-02-10 12:05:50 +0100

    Added Entity Factories
    Added EntitiesFactory Interface
    
    Added ZombiesFactory
    
    Added SunsFactory

---
+++ src/main/java/model/api/EntitiesFactory.java
@@ -0,0 +1,24 @@
+package model.api;
+
+import java.util.Set;
+
+/**
+ * Factory Pattern for Generated Entities, such as Zombies and Suns.
+ */
+public interface EntitiesFactory {
+    /**
+     * Generates an Entity.
+     * 
+     * @return the generated entity.
+     */
+    Entities createEntity();
+    
+    /**
+     * Generates a Set of Entities.
+     * Calls {@link #createEntity()} n times.
+     * 
+     * @param n number of entities to generate.
+     * @return the generated set of entities.
+     */
+    Set<Entities> createEntities(int n);
+}
---
+++ src/main/java/model/impl/SunsFactory.java
@@ -0,0 +1,22 @@
+package model.impl;
+
+import java.util.Set;
+
+import model.api.Entities;
+import model.api.EntitiesFactory;
+
+public class SunsFactory implements EntitiesFactory {
+
+    @Override
+    public Entities createEntity() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createEntity'");
+    }
+
+    @Override
+    public Set<Entities> createEntities(int n) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createEntities'");
+    }
+    
+}
