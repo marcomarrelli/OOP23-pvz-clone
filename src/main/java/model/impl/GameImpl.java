4222f0218316bf9274839f0db774fd5a8f9ab8d8
Margherita <margherita.zanchini@studio.unibo.it>
2024-02-10 18:34:17 +0100

    added a method that hadles the caso of the collision of a zombie and a plant

--- src/main/java/model/impl/GameImpl.java
+++ src/main/java/model/impl/GameImpl.java
@@ -1,6 +1,9 @@
 package model.impl;
 
 import model.api.Game;
+import model.api.Plant;
+import model.api.Zombie;
+
 import java.util.List;
 import java.util.ArrayList;
 
@@ -8,6 +11,7 @@ import java.util.ArrayList;
 public class GameImpl implements Game{
 
     private static final int DELTA=1;
+    private static final int timeRechargeAttackZombie = 2000;
 
     private List<PlantImpl> plants = new ArrayList<>();
     private List<ZombieImpl> zombies = new ArrayList<>();
@@ -48,7 +52,35 @@ public class GameImpl implements Game{
      * @author Zanchini Margherita
      */
     public void checkCollision(){
-        
+        for (ZombieImpl zombie : zombies) {
+            for (PlantImpl plant : plants) {
+                if(zombie.getPosition().getX() < plant.getPosition().getX() + DELTA){
+                    zombieEatPlant(zombie, plant);
+                }
+            }
+           
+        }
     }
-    
+
+    /**
+     * @author Zanchini Margherita
+     * @param zombie the zombie that eats the plant
+     * @param plant the plant that is eaten by the zombie
+     * 
+     * this method handles the case of collision of a zombie and a plant
+     * the plant takes damage
+     * and then we check if the plant is still alive after the damage received
+     * if not we remove it from the list of all the plants
+     *  
+     */
+    private void zombieEatPlant(Zombie zombie, Plant plant){
+        long zombieTime = zombie.getTimeRechargeAttack();
+        long currentTime = System.currentTimeMillis();
+        if( currentTime-zombieTime > timeRechargeAttackZombie){
+            plant.receiveDamage(zombie.getDamage());
+            if(!plant.isAlive()){
+                plants.remove(plant);
+            }
+        }
+    }    
 }
