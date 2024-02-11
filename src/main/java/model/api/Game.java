da572b5643d0a44dfd0a9bab4bf48aee0ae30197
Margherita <margherita.zanchini@studio.unibo.it>
2024-02-11 21:14:20 +0100

    javadoc

--- src/main/java/model/api/Game.java
+++ src/main/java/model/api/Game.java
@@ -20,6 +20,14 @@ public interface Game {
      */
     public void createPlant(Pair<Double, Double> position);
 
+    /**
+     * @param posClick is the position where the mouse has been clicked on the screen
+     * 
+     * this method check what type of click:
+     * clicked on a sun
+     * clicked on the image of the plant
+     * clicked on a cell to position the plant chosen
+     */
     public void mouseEvent(Pair<Double, Double> posClick);
 
     public void checkCollision();
