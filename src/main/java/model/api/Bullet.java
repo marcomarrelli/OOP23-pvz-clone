4433a9d7ed0b897c6211377ddd0809b5dcf08254
Margherita <margherita.zanchini@studio.unibo.it>
2024-02-13 14:40:47 +0100

    corrected checkstyle in Bullet

--- src/main/java/model/api/Bullet.java
+++ src/main/java/model/api/Bullet.java
@@ -1,16 +1,21 @@
 package model.api;
 
+/**
+ * interface for the bullet.
+ */
 public interface Bullet extends Entities {
     /**
-     * @return the speed of the bullet
+     * @return the speed of the bullet.
      */
     double getSpeed();
+
     /**
-     * @return the damage of the bullet
+     * @return the damage of the bullet.
      */
     double getDamage();
+
     /**
-     * moves the position of the bullet
+     * moves the position of the bullet.
      */
-    void move();    
+    void move();
 }
