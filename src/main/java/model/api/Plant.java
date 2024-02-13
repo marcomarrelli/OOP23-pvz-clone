28bdab721d3b8c1096270718676dbae48c001a00
Margherita <margherita.zanchini@studio.unibo.it>
2024-02-13 14:15:11 +0100

    corrected checksyle in file Plant.java

--- src/main/java/model/api/Plant.java
+++ src/main/java/model/api/Plant.java
@@ -1,9 +1,14 @@
 package model.api;
 
+/**
+ * interface for the entity plant.
+ * 
+ * @author Zanchini Margherita
+ */
 public interface Plant extends ActiveEntities {
     /**
      * @return the time in seconds between the attacks
      */
     double getTimeBetweenAttacks();
-    
+
 }
