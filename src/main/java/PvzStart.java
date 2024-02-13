d7a859f0ccc49cf1bada087de4e9674856628d99
marcomarrelli <marcomarrelli03@gmail.com>
2024-02-13 16:47:41 +0100

    Resolved CheckStyle Errors in Pair and PvzStart

--- src/main/java/PvzStart.java
+++ src/main/java/PvzStart.java
@@ -5,7 +5,7 @@ import controller.impl.ControllerImpl;
  * 
  * @author Sofia Caberletti, Margherita Zanchini, Sofia Lotti, Marco Marrelli.
  */
-public class PvzStart {
+public final class PvzStart {
     /**
      * Main method to start the Plants Vs Zombies application.
      * 
@@ -15,5 +15,9 @@ public class PvzStart {
     public static void main(final String[] args) {
         ControllerImpl controller = new ControllerImpl();
         controller.initGame();
+    }
+
+    private PvzStart() {
+        PvzStart.main(null);
     }
 }
