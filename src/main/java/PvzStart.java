3c6d2b336a39003daf3b9f7f954d705c38d30e1e
Sofia <irenesofia.lotti@studio.unibo.it>
2024-02-13 15:37:45 +0100

    Solved some problems in PvzStart

--- src/main/java/PvzStart.java
+++ src/main/java/PvzStart.java
@@ -3,10 +3,16 @@ import controller.impl.ControllerImpl;
 /**
  * Plants Vs Zombies Application's Entry Point.
  * 
- * @author Sofia Caberletti, Margherita Zanchini, Sofia Lotti, Maco Marrelli
+ * @author Sofia Caberletti, Margherita Zanchini, Sofia Lotti, Marco Marrelli.
  */
-public class PvzStart {    
-    public static void main(String[] args) {
+public class PvzStart {
+    /**
+     * Main method to start the Plants Vs Zombies application.
+     * 
+     * @param args the command-line arguments passed to the application (not used in
+     *             this case).
+     */
+    public static void main(final String[] args) {
         ControllerImpl controller = new ControllerImpl();
         controller.initGame();
     }
