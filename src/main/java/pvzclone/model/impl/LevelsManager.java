b7dae6457852b446ac08cb9478e27fc042e916d1
marcomarrelli <marcomarrelli03@gmail.com>
2024-02-15 23:42:34 +0100

    Fixed Checkstyle Errors in Level Impl/Manager

--- src/main/java/pvzclone/model/impl/LevelsManager.java
+++ src/main/java/pvzclone/model/impl/LevelsManager.java
@@ -4,9 +4,14 @@ import java.util.List;
 import java.util.Optional;
 import java.util.ArrayList;
 
-public class LevelsManager {
-    private final static int MIN_LEVELS = 1;
-    private final static int MAX_LEVELS = 5;
+/**
+ * Class used for Managing Levels in the Game.
+ * 
+ * @author Marco Marrelli
+ */
+public final class LevelsManager {
+    private static final int MIN_LEVELS = 1;
+    private static final int MAX_LEVELS = 5;
 
     private final List<LevelImpl> levelList;
 
@@ -19,62 +24,88 @@ public class LevelsManager {
     private static final long SUN_SPAWN_RATE_DECREMENT_RANGE = 25;
     private static final long ZOMBIE_SPAWN_RATE_DECREMENT_RANGE = 75;
 
-    public LevelsManager(int levelNumber) {
+    /**
+     * Constructor for LevelsManager.
+     * 
+     * @param levelNumber number of levels to generate.
+     */
+    public LevelsManager(final int levelNumber) {
+        int tempLevels = levelNumber;
+
+        if (tempLevels < MIN_LEVELS) {
+            tempLevels = MIN_LEVELS;
+        } else if (tempLevels > MAX_LEVELS) {
+            tempLevels = MAX_LEVELS;
+        }
+
         this.levelList = new ArrayList<>();
 
-        if (levelNumber < MIN_LEVELS ) {
-            levelNumber = MIN_LEVELS;
-        }
-        else if (levelNumber > MAX_LEVELS) {
-            levelNumber = MAX_LEVELS;
-        }
-
-        for (int l=0; l<levelNumber; l++) {
+        for (int l = 0; l < tempLevels; l++) {
             this.levelList.add(this.createLevel(l));
         }
     }
 
+    /**
+     * Returns the number of levels.
+     * 
+     * @return the number of levels.
+     */
     public int getLevelCount() {
         return this.levelList.size();
     }
 
-    public LevelImpl createLevel(int delta) {
+    /**
+     * Returns a new created level regarding the delta.
+     * 
+     * @param delta the index of the level in the list.
+     * @return the number of levels.
+     */
+    public LevelImpl createLevel(final int delta) {
         return new LevelImpl(
-            LevelsManager.ZOMBIE_COUNT +
-            (LevelsManager.ZOMBIE_COUNT_STEP * delta),
-            
-            (LevelsManager.ZOMBIE_COUNT +
-            (LevelsManager.ZOMBIE_COUNT_STEP * delta))
-            / LevelsManager.ZOMBIE_COUNT,
-            
-            LevelsManager.SUN_SPAWN_RATE +
-            (LevelsManager.SUN_SPAWN_RATE_STEP * delta),
-                        
-            LevelsManager.ZOMBIE_SPAWN_RATE +
-            (LevelsManager.ZOMBIE_SPAWN_RATE_STEP * delta),
-            
-            LevelsManager.SUN_SPAWN_RATE_DECREMENT_RANGE +
-            (LevelsManager.SUN_SPAWN_RATE_DECREMENT_RANGE * delta),
+                LevelsManager.ZOMBIE_COUNT
+                + (LevelsManager.ZOMBIE_COUNT_STEP * delta),
 
-            LevelsManager.ZOMBIE_SPAWN_RATE_DECREMENT_RANGE +
-            (LevelsManager.ZOMBIE_SPAWN_RATE_DECREMENT_RANGE * delta)
-        );
+                (LevelsManager.ZOMBIE_COUNT
+                + (LevelsManager.ZOMBIE_COUNT_STEP * delta))
+                / LevelsManager.ZOMBIE_COUNT,
+
+                LevelsManager.SUN_SPAWN_RATE
+                + (LevelsManager.SUN_SPAWN_RATE_STEP * delta),
+
+                LevelsManager.ZOMBIE_SPAWN_RATE
+                + (LevelsManager.ZOMBIE_SPAWN_RATE_STEP * delta),
+
+                LevelsManager.SUN_SPAWN_RATE_DECREMENT_RANGE
+                + (LevelsManager.SUN_SPAWN_RATE_DECREMENT_RANGE * delta),
+
+                LevelsManager.ZOMBIE_SPAWN_RATE_DECREMENT_RANGE
+                + (LevelsManager.ZOMBIE_SPAWN_RATE_DECREMENT_RANGE * delta));
     }
 
-    public LevelImpl getLevel(Optional<Integer> index) {
+    /**
+     * Returns the level at index inside the list.
+     * 
+     * @param index the index of the level in the list.
+     * @return the level at the given index in the list.
+     */
+    public LevelImpl getLevel(final Optional<Integer> index) {
         if (index.isEmpty()) {
             throw new IllegalStateException("No valid level selected!");
         }
 
         if (index.get() >= this.getLevelCount()) {
-            return this.getLevel(Optional.of(this.getLevelCount()-1));
-        }
-        else if (index.get() < 0) {
+            return this.getLevel(Optional.of(this.getLevelCount() - 1));
+        } else if (index.get() < 0) {
             return this.getLevel(Optional.of(0));
         }
         return this.levelList.get(index.get());
     }
 
+    /**
+     * Returns the list of levels.
+     * 
+     * @return the levels list.
+     */
     public List<LevelImpl> getLevelList() {
         if (this.levelList == null || this.levelList.isEmpty()) {
             throw new IllegalStateException("There are no valid levels to load!");
