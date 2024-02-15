79b8efda37838ff76bf03ff9f9c867b6714c8fe0
Margherita <margherita.zanchini@studio.unibo.it>
2024-02-15 19:25:47 +0100

    new test for the plant

---
+++ src/test/java/pvzclone/PlantTest.java
@@ -0,0 +1,52 @@
+package pvzclone;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import pvzclone.model.api.Plant;
+import pvzclone.model.impl.Pair;
+import pvzclone.model.impl.PlantImpl;
+
+/**
+ * this class contains the test for the plant
+ */
+public class PlantTest {
+
+    private static final double DAMAGE = 20;
+    private static final double LIFE = 100;
+    private static final String NAME = "plant";
+    private static final Pair<Integer, Integer> POS = new Pair<>(0, 0);
+    private static final long COOLDOWN = 3000;
+    private static final int COST = 100;
+
+    private Plant newPlant() {
+        return new PlantImpl(DAMAGE, LIFE, NAME, POS, COOLDOWN, COST);
+    }
+
+    @Test
+    void isPlantAliveWithCorrectValues() {
+        Plant plant = newPlant();
+        assertTrue(plant.isAlive());
+        assertEquals(DAMAGE, plant.getDamage());
+        assertEquals(LIFE, plant.getRemainingLife());
+        assertEquals(NAME, plant.getEntityName());
+        assertEquals(POS, plant.getPosition());
+        assertEquals(COOLDOWN, plant.getCooldown());
+        assertEquals(COST, plant.getPlantCost());
+    }
+
+    @Test
+    void correctDecreaseLife() {
+        Plant plant = newPlant();
+        plant.receiveDamage(DAMAGE);
+        assertTrue(plant.isAlive());
+        assertEquals(LIFE - DAMAGE, plant.getRemainingLife());
+        plant.receiveDamage(LIFE);
+        assertTrue(plant.getRemainingLife() < 0);
+        assertFalse(plant.isAlive());
+    }
+
+}
